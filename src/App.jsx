c784efe30384272535d53cab8aeb4f021821eb40
munchking98 <blood89@nate.com>
2023-01-05 04:17:34 +0900

    ✨ Feat :  IssueViewer Page route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 
 const Loading = lazy(() => import('./components/Loading'));
-
+const IssueViewer = lazy(() => import('./pages/IssueViewer'));
 const Issue = lazy(() => import('./pages/Issue'));
 
 const App = () => {
@@ -17,6 +17,14 @@ const App = () => {
           element={
             <Suspense fallback={<Loading />}>
               <Issue />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/issue/:id"
+          element={
+            <Suspense fallback={<Loading />}>
+              <IssueViewer />
             </Suspense>
           }
         />
